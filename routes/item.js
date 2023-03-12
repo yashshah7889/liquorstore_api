a8f83b1f2c0cd96eff1d56d4f2258ce2a1915085
yash shah <yashshah2498@gmail.com>
2023-03-12 16:20:36 -0400

    search by brand added

--- routes/item.js
+++ routes/item.js
@@ -10,17 +10,54 @@ items.post('/items',(req,res) =>{
     let pname = req.body.pname;
     let brand = req.body.brand;
 
-    
-    Products.findAll({
+    if(brand.length>0){
+        Products.findAll({
         where: { brand : brand }
-    }).then(async(product)=>{
-
+        })
+        .then((data)=>{   
+            if(data.length>0)
+            {
+                let dataArray = [];
+                data.foreach((productData,index)=>{
+                    let pd ={};
+                    pd.id=index;
+                    pd.pname = productData.pname;
+                    pd.alcPercent = productData.alcPercent;
+                    pd.size = productData.size;
+                    pd.category = productData.category;
+                    pd.subcategory = productData.subcategory;
+                    pd.brand = productData.brand;
+                    pd.price = productData.price;
+                    pd.pic = productData.pic;
+                    dataArray.push(pd);
+                });
+                res.status(200).json({ 
+                    status: "Success",
+                    status_msg: "Data found",
+                    dataArray: dataArray
+                });
+                return true;
+            }
+            else
+            {
+                res.status(200).json({ 
+                    status: "Success",
+                    status_msg: "No data found"
+                });
+                return true;
+            }
+        })
+        .catch((error)=>{
+            console.log("error",error);
+            res.status(400).json({ 
+                status: "Error",
+                status_msg: "Oops! something went wrong in getting data"
+            });
+            return false;
+        });
     }
-
-    )
-
 });
-
+module.exports = items;
 
 
 
